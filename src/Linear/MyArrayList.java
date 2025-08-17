9c099c64ed67ad41805dd6fcb6568867397450d5
Renato <rpgouveia@gmail.com>
2025-08-17 16:28:01 -0300

    feat: add MyArrayList class

---
+++ src/Linear/MyArrayList.java
@@ -0,0 +1,23 @@
+package Linear;
+
+// Create generic (https://www.w3schools.com/java/java_generics.asp)
+
+// Implementation
+public class MyArrayList<T> {
+    // attributes
+    private T[] data;
+    private int capacity;
+
+    // constructor
+    public MyArrayList(int capacity) {
+        this.capacity = capacity;
+        this.data = new data[capacity];
+    }
+
+    // methods
+    private <E> void add(E e) {
+        // todo
+    }
+
+}
+
